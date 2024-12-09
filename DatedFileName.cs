051b2ccddcc0c04ecf64e550be45affcb81f1a22
thines01 <thines01@yahoo.com>
2024-12-09 16:12:26 -0600

    Add project files.

---
+++ DatedFileName.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace DatedFileName
+{
+    public partial class CDatedFileName
+    {
+       /// <summary>
+       /// Converts 
+       ///    "c:/science/data/fred .txt . txt"
+       ///    to
+       ///    "c:/science/data/fred.txt.20150507161045.txt"
+       /// </summary>
+       public static FileInfo GetDatedFileName(string strFileName)
+       {
+         return GetDatedFileName(strFileName, DateTime.Now);
+       }
+
+      /// <summary>
+      /// Converts 
+      ///    "c:/science/data/fred .txt . txt"
+      ///    to
+      ///    "c:/science/data/fred.txt.20150507161045.txt"
+      /// </summary>
+      public static FileInfo GetDatedFileName(string strFileName, DateTime dt)
+      {
+         FileInfo fi = new FileInfo(strFileName);
+
+         string[] arr_strFileParts =
+            fi.Name.Split('.').Select(strFilePart => strFilePart.Trim()).ToArray();
+
+         return
+            new FileInfo(
+               Path.Combine(fi.Directory.FullName,
+                  string.Join(".",
+                     arr_strFileParts.Reverse().Skip(1).Reverse()
+                     .Concat(new string[]
+                       {
+                           dt.ToString("yyyyMMddHHmmss"),
+                           arr_strFileParts.Last()
+                       }).ToArray())));
+      }
+   }
+}
---
+++ GetSimpleDateFormat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DatedFileName
+{
+	public partial class CDatedFileName
+	{
+		/// <summary>
+		/// Returns YYYYMMDD or YYYY(delim)MM{delim}DD 
+		/// </summary>
+		/// <param name="dtWhen">DateTime to use</param>
+		/// <param name="strDelim">Delimiter to use or null</param>
+		/// <returns></returns>
+		public static string GetSimpleDateFormat(DateTime dtWhen, string strDelim = "")
+		{
+			return string.Format("{1}{0}{2}{0}{3}",
+				strDelim,
+				dtWhen.Year.ToString("D4"),
+				dtWhen.Month.ToString("D2"),
+				dtWhen.Day.ToString("D2")); // call this from the immediate window.
+		}
+	}
+}
